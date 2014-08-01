99b3b5e8f48ce027b90dd4ee9c7c45098ca47ddc
n0ts <me@n0ts.org>
2014-08-02 01:01:20 +0900

    Fix launch_on_login is not working

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,12 +9,24 @@
 #   launch_on_login - will automatically launch and login. Defaults to
 #                     true. Set this to false if you want to avoid
 #                     automatically launching.
-class karabiner {
+class karabiner(
+  $launch_on_login = true,
+) {
   include karabiner::config
 
   package { "Karabiner_${karabiner::config::version}":
     ensure   => installed,
     source   => $karabiner::config::dmg_url,
     provider => 'pkgdmg'
+  }
+
+
+  $launch_on_login_ensure = $launch_on_login ? {
+    true    => present,
+    default => absent
+  }
+
+  class { 'karabiner::login_item':
+    ensure => $launch_on_login_ensure
   }
 }
